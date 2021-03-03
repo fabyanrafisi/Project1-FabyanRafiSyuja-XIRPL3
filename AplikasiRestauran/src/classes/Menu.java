ac8e17b09b8cd68858ce035c551b9431bae6eaaa
fabyanrafisi <fabyanrafisi@gmail.com>
2021-03-03 15:49:33 +0700

    Implementasi Struktur Kelas Aplikasi Kasir

---
+++ AplikasiRestauran/src/classes/Menu.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author Hp user
+ */
+public class Menu {
+    public String nama_menu;
+    public double harga;
+    public String kategori;
+    
+    public String getNama_menu(){
+        return "";
+    }
+    
+    public void setNama_menu(String nama_menu){
+      
+    }
+    
+    public double getHarga(){
+        return 0;
+    }
+    
+    void setHarga(double Harga){
+        
+    }
+    
+    public String getKategori(){
+        return "";
+    }
+    
+    public void setKategori(String kategori){
+        
+    }
+}
