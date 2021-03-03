d3310bb82de1926f302288d500b1d6433ef0761c
fabyanrafisi <fabyanrafisi@gmail.com>
2021-03-03 16:25:24 +0700

    Implementasi Kelas Daftar Menu

--- AplikasiRestauran/src/classes/DaftarMenu.java
+++ AplikasiRestauran/src/classes/DaftarMenu.java
@@ -1,4 +1,4 @@
-/*
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -15,18 +15,29 @@ public class DaftarMenu {
     private ArrayList<Menu> daftarMenu;
     
     public DaftarMenu(){
-        
+        daftarMenu = new ArrayList<>();
     }
     
     public void tambahMenu(Menu menu){
-        
+        daftarMenu.add(menu);
     }
     
     public void getMenuByKategori(String kategori){
+        System.out.println("========= " + kategori + " ========");
         
+        for(int i = 0; i < daftarMenu.size(); i++){
+            Menu m = daftarMenu.get(i);
+            if(m.getKategori().equals(kategori)){
+                System.out.println((i+1) + ". " + m.getNama_menu() + "\t" + m.getHarga());
+            }
+        }
     }
     
     public void tampilDaftarMenu(){
-        
+        System.out.println("========= Krusty Ramen =========");
+        getMenuByKategori("Ramen");
+        getMenuByKategori("Kuah");
+        getMenuByKategori("Toping");
+        getMenuByKategori("Minuman");
     }
 }
